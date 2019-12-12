73e45345b4225293e3377776be98b7910702fabb
Oshi <48289602+OshiTaiya@users.noreply.github.com>
2019-12-11 20:25:25 -0500

    Add files via upload

---
+++ online-library/inc/data2.php
@@ -0,0 +1,177 @@
+<?php
+$catalog = [];
+//Books
+$catalog[101] = [
+ "title" => "Go Ask Alice",
+ "img" => "img/media/AskAlice.jpg",
+"genre" => "Young Adult Fiction",
+"format" => "Paperback",
+"year" => 2006,
+"category" => "Books",
+"authors" => [
+    "A Simon & Schuster author"
+],
+"publisher" => "Simon Pulse",
+"isbn" => '9781416914631'
+];
+
+$catalog[102] = [
+ "title" => "The Face On The Milk Carton",
+ "img" => "img/media/MilkCarton.jpg",
+"genre" => "Fantasy Fiction",
+"format" => "Paperback",
+"year" => 2012,
+"category" => "Books",
+"authors" => [
+    "Caroline B. Cooney"
+],
+"publisher" => "Ember",
+"isbn" => '9780385742382'
+];
+
+$catalog[103] = [
+ "title" => "The Book of Joy",
+ "img" => "img/media/Parable.jpg",
+"genre" => "Dystopian Fiction",
+"format" => "Paperback",
+"year" => 2000,
+"category" => "Books",
+"authors" => [
+    "Octavia E. Butler"
+],
+"publisher" => " Grand Central Publishing",
+"isbn" => '9780446675505'
+];
+
+$catalog[104] = [
+ "title" => "Speak",
+ "img" => "img/media/Speak.jpg",
+"genre" => "Fiction",
+"format" => "Paperback",
+"year" => 2011,
+"category" => "Books",
+"authors" => [
+    "Laurie Anderson"
+],
+"publisher" => "Farrar, Straus and Giroux",
+"isbn" => '9780312674397'
+];
+
+
+//Movies
+$catalog[201] = [
+"title" => "Full Metal Jacket",
+"img" => "img/media/FullMetal.jpg",
+"genre" => "Action, Adventure",
+"format" => "Stream",
+"year" => 1987,
+"category" => "Movies",
+"director" => "Stanley Kubrick",
+"writers" => [
+    "Stanley Kubrick",
+    "Michael Herr"
+],
+"stars" => [
+    "Matthew Modine",
+    "R. Lee Ermey",
+    "Vincent D’Onofrio"
+]
+];
+
+$catalog[202] = [
+"title" => "Friday the 13th",
+"img" => "img/media/Fri13.jpg",
+"genre" => "Mystery, Slasher",
+"format" => "Stream",
+"year" => 1980,
+"category" => "Movies",
+"director" => "Sean S. Cunningham",
+"writers" => [
+    "Victor Miller"
+],
+"stars" => [
+    "Betsy Palmer",
+    "Adrienne King",
+    "Jeannine Taylor"]
+];
+
+$catalog[203] = [
+"title" => "The Evil Dead",
+"img" => "img/media/EvilDead.jpg",
+"genre" => "Supernatural,Horror,Fantasy",
+"format" => "Stream",
+"year" => 1983,
+"category" => "Movies",
+"director" => "Sam Raimi",
+"writers" => [
+    "Sam Raimi"
+],
+"stars" => [
+    "Bruce Campbell",
+    "Ellen Sandweiss",
+    "Richard DeManincor"
+]
+];
+
+
+$catalog[204] = [
+"title" => "Pacific Rim",
+"img" => "img/media/PaciRim.jpg",
+"genre" => "Action, Adventure, Sci-fi",
+"format" => "Stream",
+"year" => 2013,
+"category" => "Movies",
+"director" => "Guillermo del Toro",
+"writers" => [
+    "Guillermo del Toro",
+    "Travis Beacham"
+],
+"stars" => [
+    "Idris Elba",
+    "Charlie Hunnam",
+    "Rinko Kikuchi"
+]
+];
+
+
+//Music
+$catalog[301] = [
+"title" => "In Rainbows",
+"img" => "img/media/InRainbows.jpg",
+"genre" => "Alternative Rock",
+"format" => "CD",
+"year" => 2008,
+"category" => "Music",
+"artist" => "Radiohead"
+];
+
+$catalog[302] = [
+"title" => "Anti",
+"img" => "img/media/Anti.png",
+"genre" => "Hip Hop",
+"format" => "Stream",
+"year" => 2016,
+"category" => "Music",
+"artist" => "Rihanna"
+];
+
+$catalog[303] = [
+"title" => "Free Spirit",
+"img" => "img/media/FreeSpirit.jpg",
+"genre" => "R&B",
+"format" => "Stream",
+"year" => 2019,
+"category" => "Music",
+"artist" => "Khalid"
+];
+
+$catalog[304] = [
+"title" => "Gypsum Strings",
+"img" => "img/media/GypStrings.jpg",
+"genre" => "Rock",
+"format" => "CD",
+"year" => 2006,
+"category" => "Music",
+"artist" => "Oakley Hall"
+];
+ ?>
